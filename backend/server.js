26fb26a27313e434f267b653ea4fc69898201f7a
riddham <riddhamraiyani123@gmail.com>
2025-09-20 17:59:20 +0530

    Backend start and setup and create adminRoute nad userRoute

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express, { response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminroute.js";
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoint
+app.use('/api/admin', adminRouter)
+
+
+app.get("/", (req, res) => {
+  res.send("api working ");
+});
+
+app.listen(port, () => console.log("Server strated", port));
