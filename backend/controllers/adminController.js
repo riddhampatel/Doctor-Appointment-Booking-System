45da6da7c9cb318c7dff8e8cb5e0a1654f926d24
riddham <riddhamraiyani123@gmail.com>
2025-09-25 18:07:53 +0530

    .

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
+import jwt from 'jsonwebtoken'
 
 // API for adding doctor
 const addDoctor = async (req, res) => {
@@ -86,4 +87,25 @@ const addDoctor = async (req, res) => {
   }
 };
 
-export { addDoctor };
+// API For admin login
+
+const loginAdmin = async (req,res) => {
+  try {
+      
+        const {email,password} =req.body
+        if (email === process.env.ADMIN_EMAIL && process.env.ADMIN_PASSWORD){
+           
+          const token = jwt.sign(email+password,process.env.JWT_SECRET)
+          res.json({success:true,token})
+        }
+        else {
+          res.json({success:false, message:"Invalid creadentials"})
+        }
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+}
+
+export { addDoctor , loginAdmin};
