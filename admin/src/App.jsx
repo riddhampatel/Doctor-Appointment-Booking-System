0ccc8b66a8d6ee8db16ff855dfb2b3884a94d43d
riddham <riddhamraiyani123@gmail.com>
2025-10-12 10:18:06 +0530

    login.jsx working

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/reactToastify.css';
 
 const App = () => {
   return (
     <div>
       <Login />
+      <ToastContainer/>
     </div>
   )
 }
